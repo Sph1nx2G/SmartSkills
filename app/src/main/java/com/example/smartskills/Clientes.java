d24743f73b0740c8d4c67316d8b0ec6af11733bc
Esfenge <46057783+Sph1nx2G@users.noreply.github.com>
2021-06-24 09:02:51 +0100

    Clientes visual ja feito

--- app/src/main/java/com/example/smartskills/Clientes.java
+++ app/src/main/java/com/example/smartskills/Clientes.java
@@ -2,13 +2,55 @@ package com.example.smartskills;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class Clientes extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Clientes extends AppCompatActivity implements View.OnClickListener {
+
+    public Button btnNovoCliente;
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clientes);
+
+        listView = (ListView)findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("André");
+        arrayList.add("António");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+
+        btnNovoCliente =(Button)findViewById(R.id.NovoCliente);
+
+        btnNovoCliente.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.NovoCliente:
+                i = new Intent(this, Cliente.class);
+                startActivity(i);
+                break;
+
+        }
+    }
+
 }
